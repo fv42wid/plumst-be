1a701649686e5161db4b75595630bbde857622ef
Frank Vinci <fv42wid@yahoo.com>
2017-02-26 20:48:20 +0000

    finished messages.create method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,9 @@ class MessagesController < ApplicationController
   def create
     @message = Message.build(message_params)
     if @message.save
-
+      render json: @message, status: :created, location: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
     end
   end
 
